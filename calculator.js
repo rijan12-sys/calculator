c3d6b2695d57850a91c3d99e1c9c2bcb2e44f368
rijan12-sys <manandhar_rijan@yahoo.com>
2022-01-05 13:12:55 +1100

    first commit

---
+++ calculator.js
@@ -0,0 +1,183 @@
+$(document).ready(function(){
+
+     //zero digit clicked
+     $("#zero").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#zero").text())
+    })
+    //one digit clicked
+    $("#one").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#one").text())
+    })
+    //two digit clicked
+    $("#two").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#two").text())
+    })
+    //three digit clicked
+    $("#three").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#three").text())
+    })
+    //four digit clicked
+    $("#four").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#four").text())
+    })
+    //five digit clicked
+    $("#five").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#five").text())
+    })
+    //six digit clicked
+    $("#six").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#six").text())
+    })
+    //seven digit clicked
+    $("#seven").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#seven").text())
+    })
+    //eight digit clicked
+    $("#eight").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#eight").text())
+    })
+    //nine digit clicked
+    $("#nine").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#nine").text())
+    })
+    //plus digit clicked
+    $("#plus").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#plus").text())
+    })
+    //minus digit clicked
+    $("#minus").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#minus").text())
+    })
+    //divide digit clicked
+    $("#divide").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#divide").text())
+    })
+    //percent digit clicked
+    $("#percent").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#percent").text())
+    })
+    //dot digit clicked
+    $("#dot").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#dot").text())
+    })
+    //dot digit clicked
+    $("#multiply").click(function(){
+        $(".textArea").val($(".textArea").val()+ $("#multiply").text())
+    })
+    //AC digit clicked
+    $("#AC").click(function(){
+        $(".textArea").val(" ")
+    })
+    //equal button clicked
+    $("#equal").click(function(){ 
+    })
+
+
+
+    $("#equal").click(function(){
+       
+        $(".textArea").val(eval($(".textArea").val()))
+    })
+
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $("#divide").click(function(){
+    //     $(".textArea").text("/")
+    // })
+    // $("#percent").click(function(){
+    //     $(".textArea").text("%")
+    // })
+    // // $("#plusMinus").click(function(){
+    // //     $(".textArea").text("")
+    // // })
+    // $("#seven").click(function(){
+    //     $(".textArea").text("7")
+    // })
+    // $("#eight").click(function(){
+    //     $(".textArea").text("8")
+    // })
+    // $("#nine").click(function(){
+    //     $(".textArea").text("9")
+    // })
+    // $("#x").click(function(){
+    //     $(".textArea").text("x")
+    // })
+    // $("#four").click(function(){
+    //     $(".textArea").text("4")
+    // })
+    // $("#five").click(function(){
+    //     $(".textArea").text("5")
+    // })
+    // $("#six").click(function(){
+    //     $(".textArea").text("6")
+    // })
+    // $("#minus").click(function(){
+    //     $(".textArea").text("-")
+    // })
+    // $("#one").click(function(){
+    //     $(".textArea").text("1")
+    // })
+    // $("#two").click(function(){
+    //     $(".textArea").text("2")
+    // })
+    // $("#three").click(function(){
+    //     $(".textArea").text("3")
+    // })
+    // $("#plus").click(function(){
+    //     $(".textArea").text("+")
+    // })
+    // $("#zero").click(function(){
+    //     $(".textArea").text("0")
+    // })
+    // $("#dot").click(function(){
+    //     $(".textArea").text(".")
+    // })
+
+
+
+   
+
+
+    // $(".calc-btn").click(function(){
+    //     $(".textArea").val($(".textArea").val() + $(this).html())
+    // })
+
+
+    // $(".clear").click(function(){
+    //     $(".textArea").val(" ")
+    // })
+
+
+    
